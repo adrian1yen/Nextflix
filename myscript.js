e7020af9782dd1d2391b0d6a83b1bed18d640ddf
hijka <adrian1yen@yahoo.com>
2017-09-26 22:37:52 -0700

    version 1.1

--- myscript.js
+++ myscript.js
@@ -4,34 +4,36 @@ chrome.storage.sync.get({
 	nextFlixNextEpisode: true,
 	nextFlixNextEpisodeTime: 0
 }, function(items) {
+	// Settings
 	var skipIntro = items.nextFlixSkipIntro;
 	var skipIntroTime = items.nextFlixSkipIntroTime;
 	var nextEpisode = items.nextFlixNextEpisode;
 	var nextEpisodeTime = items.nextFlixNextEpisodeTime;
+
 	var ancestor = $('body');
+
+	// Classes
+	var skipIntroClasses = ['.skip-credits'];
+	var nextEpisodeClasses = ['.player-postplay-still-hover-container', '.WatchNext-still-container'];
+
+	// Observer Method
 	var ob = new MutationObserver(function() {
-		if($('.skip-credits').length > 0 && skipIntro) {
+		// Skip intro
+		if($(skipIntroClasses.join()).length > 0 && skipIntro) {
 			setTimeout(function() {
-				if($('.skip-credits').length > 0) {
-					$('.skip-credits').find('a')[0].click();
+				if($(skipIntroClasses.join()).length > 0) {
+					$(skipIntroClasses.join()).find('a')[0].click();
 				}
 			}, skipIntroTime * 1000)
 		}
-		//if($('.player-postplay-still-hover-container').length > 0 && nextEpisode) {
-			//console.log('what');
-			//setTimeout(function() {
-				//if($('.player-postplay-still-hover-container').length > 0) {
-					//console.log('huh');
-					//$('.player-postplay-still-hover-container')[0].click();
-				//} else {
-					//console.log('no click');
-				//}
-			//}, nextEpisodeTime * 1000)
-		//} else {
-			//console.log($('.player-postplay-still-hover-container').length);
-		//}
-		if($('.player-postplay-still-hover-container').length > 0) {
-			$('.player-postplay-still-hover-container')[0].click();
+
+		// Next Episode
+		if($(nextEpisodeClasses.join()).length > 0 && nextEpisode) {
+			setTimeout(function() {
+				if($(nextEpisodeClasses.join()).length > 0) {
+					$(nextEpisodeClasses.join())[0].click();
+				}
+			}, nextEpisodeTime * 1000)
 		}
 	});
 	ob.observe(ancestor[0], {
--- options.js
+++ options.js
@@ -1,43 +1,47 @@
 // Saves options to chrome.storage
 function save_options() {
-  var skipIntro = document.getElementById('skip_intro').checked;
-  var skipIntroTime = document.getElementById('skip_intro_time').value;
-  skipIntroTime = skipIntroTime > 3 ? 3 : skipIntroTime;
-  var nextEpisode = document.getElementById('next_episode').checked;
-  var nextEpisodeTime = document.getElementById('next_episode_time').value;
-  nextEpisodeTime = nextEpisodeTime > 10 ? 10 : nextEpisodeTime;
-  chrome.storage.sync.set({
-    nextFlixSkipIntro: skipIntro,
-    nextFlixSkipIntroTime: skipIntroTime,
-	nextFlixNextEpisode: nextEpisode,
-	nextFlixNextEpisodeTime: nextEpisodeTime
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-	document.getElementById('skip_intro_time').value = skipIntroTime;
-	document.getElementById('next_episode_time').value = nextEpisodeTime;
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+	// Get settings
+	var skipIntro = document.getElementById('skip_intro').checked;
+	var skipIntroTime = document.getElementById('skip_intro_time').value;
+	skipIntroTime = skipIntroTime > 3 ? 3 : skipIntroTime;
+	skipIntroTime = skipIntroTime < 0? 0 : skipIntroTime;
+	var nextEpisode = document.getElementById('next_episode').checked;
+	var nextEpisodeTime = document.getElementById('next_episode_time').value;
+	nextEpisodeTime = nextEpisodeTime > 10 ? 10 : nextEpisodeTime;
+	nextEpisodeTime = nextEpisodeTime < 0 ? 0 : nextEpisodeTime;
+
+	chrome.storage.sync.set({
+		nextFlixSkipIntro: skipIntro,
+		nextFlixSkipIntroTime: skipIntroTime,
+		nextFlixNextEpisode: nextEpisode,
+		nextFlixNextEpisodeTime: nextEpisodeTime
+	}, function() {
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		document.getElementById('skip_intro_time').value = skipIntroTime;
+		document.getElementById('next_episode_time').value = nextEpisodeTime;
+		status.textContent = 'Options saved.';
+		setTimeout(function() {
+			status.textContent = '';
+		}, 750);
+	});
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    nextFlixSkipIntro: true,
-    nextFlixSkipIntroTime: 0,
-	nextFlixNextEpisode: true,
-	nextFlixNextEpisodeTime: 0
-  }, function(items) {
-    document.getElementById('skip_intro').checked = items.nextFlixSkipIntro;
-    document.getElementById('skip_intro_time').value = items.nextFlixSkipIntroTime;
-    document.getElementById('next_episode').checked = items.nextFlixNextEpisode;
-    document.getElementById('next_episode_time').value = items.nextFlixNextEpisodeTime;
-  });
+	// Restores select box and checkbox state using the preferences
+	// stored in chrome.storage.
+	function restore_options() {
+		// Use default value color = 'red' and likesColor = true.
+		chrome.storage.sync.get({
+		nextFlixSkipIntro: true,
+		nextFlixSkipIntroTime: 0,
+		nextFlixNextEpisode: true,
+		nextFlixNextEpisodeTime: 0
+	}, function(items) {
+		document.getElementById('skip_intro').checked = items.nextFlixSkipIntro;
+		document.getElementById('skip_intro_time').value = items.nextFlixSkipIntroTime;
+		document.getElementById('next_episode').checked = items.nextFlixNextEpisode;
+		document.getElementById('next_episode_time').value = items.nextFlixNextEpisodeTime;
+	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
