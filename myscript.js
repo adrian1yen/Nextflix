b4d50211263f59a639da3bbab63c4339bfa3ebae
hijka <adrian1yen@yahoo.com>
2017-11-21 12:31:02 -0800

    Added clicking flag.

--- myscript.js
+++ myscript.js
@@ -12,16 +12,27 @@ chrome.storage.sync.get({
 	var skipIntroClasses = ['.skip-credits'];
 	var nextEpisodeClasses = ['.player-postplay-still-hover-container', '.WatchNext-still-container'];
 
+	// Variables
+	var clicking = false;
+
 	// Observer Method
 	var ob = new MutationObserver(function() {
 		// Skip intro
-		if($(skipIntroClasses.join()).length > 0 && skipIntro) {
+		if($(skipIntroClasses.join()).length > 0 && skipIntro && !clicking) {
+			clicking = true;
 			$(skipIntroClasses.join()).find('a')[0].click();
+			setTimeout(function() {
+				clicking = false;
+			}, 500);
 		}
 
 		// Next Episode
-		if($(nextEpisodeClasses.join()).length > 0 && nextEpisode) {
+		if($(nextEpisodeClasses.join()).length > 0 && nextEpisode && !clicking) {
+			clicking = true;
 			$(nextEpisodeClasses.join())[0].click()
+			setTimeout(function() {
+				clicking = false;
+			}, 500);
 		}
 	});
 	ob.observe(ancestor[0], {
