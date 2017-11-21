5e426004ec18bad41cd72306479bc0109c44f563
hijka <adrian1yen@yahoo.com>
2017-11-21 12:06:25 -0800

    Removed timeouts

--- myscript.js
+++ myscript.js
@@ -1,14 +1,10 @@
 chrome.storage.sync.get({
 	nextFlixSkipIntro: true,
-	nextFlixSkipIntroTime: 0,
 	nextFlixNextEpisode: true,
-	nextFlixNextEpisodeTime: 0
 }, function(items) {
 	// Settings
 	var skipIntro = items.nextFlixSkipIntro;
-	var skipIntroTime = items.nextFlixSkipIntroTime;
 	var nextEpisode = items.nextFlixNextEpisode;
-	var nextEpisodeTime = items.nextFlixNextEpisodeTime;
 
 	var ancestor = $('body');
 
@@ -20,20 +16,12 @@ chrome.storage.sync.get({
 	var ob = new MutationObserver(function() {
 		// Skip intro
 		if($(skipIntroClasses.join()).length > 0 && skipIntro) {
-			setTimeout(function() {
-				if($(skipIntroClasses.join()).length > 0) {
-					$(skipIntroClasses.join()).find('a')[0].click();
-				}
-			}, skipIntroTime * 1000)
+			$(skipIntroClasses.join()).find('a')[0].click();
 		}
 
 		// Next Episode
 		if($(nextEpisodeClasses.join()).length > 0 && nextEpisode) {
-			setTimeout(function() {
-				if($(nextEpisodeClasses.join()).length > 0) {
-					$(nextEpisodeClasses.join())[0].click();
-				}
-			}, nextEpisodeTime * 1000)
+			$(nextEpisodeClasses.join())[0].click()
 		}
 	});
 	ob.observe(ancestor[0], {
--- options.js
+++ options.js
@@ -2,24 +2,14 @@
 function save_options() {
 	// Get settings
 	var skipIntro = document.getElementById('skip_intro').checked;
-	var skipIntroTime = document.getElementById('skip_intro_time').value;
-	skipIntroTime = skipIntroTime > 3 ? 3 : skipIntroTime;
-	skipIntroTime = skipIntroTime < 0? 0 : skipIntroTime;
 	var nextEpisode = document.getElementById('next_episode').checked;
-	var nextEpisodeTime = document.getElementById('next_episode_time').value;
-	nextEpisodeTime = nextEpisodeTime > 10 ? 10 : nextEpisodeTime;
-	nextEpisodeTime = nextEpisodeTime < 0 ? 0 : nextEpisodeTime;
 
 	chrome.storage.sync.set({
 		nextFlixSkipIntro: skipIntro,
-		nextFlixSkipIntroTime: skipIntroTime,
 		nextFlixNextEpisode: nextEpisode,
-		nextFlixNextEpisodeTime: nextEpisodeTime
 	}, function() {
 		// Update status to let user know options were saved.
 		var status = document.getElementById('status');
-		document.getElementById('skip_intro_time').value = skipIntroTime;
-		document.getElementById('next_episode_time').value = nextEpisodeTime;
 		status.textContent = 'Options saved.';
 		setTimeout(function() {
 			status.textContent = '';
@@ -33,14 +23,10 @@ function save_options() {
 		// Use default value color = 'red' and likesColor = true.
 		chrome.storage.sync.get({
 		nextFlixSkipIntro: true,
-		nextFlixSkipIntroTime: 0,
 		nextFlixNextEpisode: true,
-		nextFlixNextEpisodeTime: 0
 	}, function(items) {
 		document.getElementById('skip_intro').checked = items.nextFlixSkipIntro;
-		document.getElementById('skip_intro_time').value = items.nextFlixSkipIntroTime;
 		document.getElementById('next_episode').checked = items.nextFlixNextEpisode;
-		document.getElementById('next_episode_time').value = items.nextFlixNextEpisodeTime;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
